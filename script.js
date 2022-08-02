f66419896db8a9610d96176dccfd5d3c03c17fc8
Shivusangam <109031337+Shivusangam@users.noreply.github.com>
2022-08-02 22:40:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let countdown = document.querySelector(".countdown");
+let time = 10;
+countdown.innerText = time--;
+setTimeout(() => {
+  countdown.innerText = time--;
+  setTimeout(() => {
+    countdown.innerText = time--;
+    setTimeout(() => {
+      countdown.innerText = time--;
+      setTimeout(() => {
+        countdown.innerText = time--;
+        setTimeout(() => {
+          countdown.innerText = time--;
+          setTimeout(() => {
+            countdown.innerText = time--;
+            setTimeout(() => {
+              countdown.innerText = time--;
+              setTimeout(() => {
+                countdown.innerText = time--;
+            }, 1000);
+                setTimeout(() => {
+                  countdown.innerText = time--;
+                
+                setTimeout(() => {
+                  countdown.innerText = "Happy Independence Day";
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
